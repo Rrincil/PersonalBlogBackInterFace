15c255133ed64b2ebb3d9f61f6a02408e425a551
xiaowang <2805528068@qq.com>
2022-06-19 17:15:33 +0800

    测试接口

--- routers/api/users.js
+++ routers/api/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const users = require('../../moudels/user')
 const Route = express.Router()
 /**
- * @router api/users
+ * @router get api/users
  * @desc 测试接口
  * @access public
  */
@@ -10,6 +10,11 @@ Route.get('/',(req,res)=>{
   res.json({mes:'users'})
 })
 
+/**
+ * @router get api/users/allmes
+ * @desc 返回所有数据
+ * @access public
+ */
 Route.get('/allmes',(req,res)=>{
   users.find().then(mes=>{
     res.json({mes})
