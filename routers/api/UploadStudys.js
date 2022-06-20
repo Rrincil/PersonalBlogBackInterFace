68ae377290f56322010e1a594ccf5dde87c129bd
xiaowang <2805528068@qq.com>
2022-06-20 21:31:28 +0800

    上传博客接口

--- routers/api/UploadStudys.js
+++ routers/api/UploadStudys.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Route = express.Router()
 const fs = require('fs')
+const multer=require('multer'); 
+const upload=multer({dest:'uploads/'})
+const path = require('path')
 const UploadStudys = require('../../moudels/UploadStudys')
 Route.get('/',(req,res)=>{
   res.json({mes:'UploadStudys'})
@@ -10,21 +13,32 @@ Route.get('/',(req,res)=>{
  * @desc 插入数据
  * @assess public 
  */
-Route.post('/insertmes',(req,res)=>{
-  console.log(req.body);
+Route.post('/insertmes',upload.single('mdfiles'),(req,res)=>{
+  console.log(req.file);
+  console.log(req.filename);
+
   // 读取文件
-  fs.read(req.body.file,(err,data)=>{
+  fs.readFile(req.file.path,(err,data)=>{
     if(!err){
       // 创建文件
       console.log(data);
-      fs.open("./static"+req.filename,'w',(err,fd)=>{
+      // fs.open("./static"+req.filename,'w',(err,fd)=>{
+      //   if(!err){
+      //     // 写入文件,插入返回的数据
+      //     fs.write(fd,data,(err,fd)=>{
+      //       console.log("存入成功");
+      //       // res.json({mes:"存入成功"})
+      //       // fs.close(fd)
+      //     })
+      //   }else{
+      //     console.log(err);
+      //   }
+      // })
+      let extpname = '.md'
+      let keepname = req.file.originalname
+      fs.writeFile(path.join(__dirname,'../../static/md/'+keepname),data,(err)=>{
         if(!err){
-          // 写入文件,插入返回的数据
-          fs.write(fd,data,(err,fd)=>{
-            console.log("插入成功");
-            res.json({mes:"插入成功"})
-            fs.close(fd)
-          })
+          res.json({path:`http://localhost:3001/md/${keepname}`})
         }else{
           console.log(err);
         }
